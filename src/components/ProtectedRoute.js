8e0055fcd79a5b9d669ea8ba3087017527100f76
Nithin <dathu@Nithinss-MacBook-Pro.local>
2024-12-01 13:56:21 -0500

    adding all files

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+import ARView from './pages/ARView';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/ar" element={<ARView />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
